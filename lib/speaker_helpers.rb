d0f1cf738e8870b47161d198d6ebe16d9ace1e04
Chris Eidhof <chris@eidhof.nl>
2015-12-18 17:31:42 +0100

    Site updated to 0c5d5c6

--- lib/speaker_helpers.rb
+++
@@ -1,7 +0,0 @@
-module SpeakerHelpers
-  def speaker_page(name)
-    require 'middleman-blog/uri_templates'
-    slug = Middleman::Blog::UriTemplates.safe_parameterize name
-    "/speakers/#{slug}"
-  end
-end
