36d23c6ec46215e7b168dd4fc1aaa339544925e4
Johan K. Jensen <jj@johanjensen.dk>
2015-12-19 19:15:15 +0100

    Add lazy load of YouTube videos

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,32 @@
 //= require_tree .
+function lazyLoadVideos() {
+    'use strict';
+    var videos = document.getElementsByClassName("embed-video");
+
+    for (var i = 0; i < videos.length; i++) {
+        // Set the thumbnail image
+        videos[i].style.backgroundImage = "url(" + videos[i].getAttribute("data-thumbnail") + ")";
+
+        // Overlay the Play icon to make it look like a video player
+        var play = document.createElement("div");
+        play.setAttribute("class", "play");
+        videos[i].appendChild(play);
+
+        videos[i].onclick = function(e) {
+            // Allow opening the video in a new tab by Ctrl- (Windows) or Cmd- (OS X) clicking the video.
+            if (e.ctrlKey || e.metaKey) {
+                return true;
+            }
+            e.preventDefault(); // Otherwise, don't open the link
+
+            // Create an iframe for the embedded player
+            var iframe = document.createElement("iframe");
+            iframe.setAttribute("src", this.getAttribute("data-url"));
+            iframe.setAttribute("frameborder", "0");
+
+            // Replace the thumbnail with the player
+            this.parentNode.replaceChild(iframe, this);
+        };
+    }
+}
+document.addEventListener("DOMContentLoaded", lazyLoadVideos);
--- source/lib/video_helpers.rb
+++ source/lib/video_helpers.rb
@@ -17,12 +17,14 @@ module VideoHelpers
 
   def embed_video(video)
     if video.youtube
-      url = "https://www.youtube.com/embed/#{video.youtube}"
-    elsif video.vimeo
-      url = "https://player.vimeo.com/video/#{video.vimeo}"
-    elsif video.wwdc
-      url = "https://developer.apple.com/videos/play/#{video.wwdc}"
+      "<a href=\"https://www.youtube.com/watch?v=#{video.youtube}\" class=\"embed-video\" data-url=\"https://www.youtube.com/embed/#{video.youtube}?autoplay=1&autohide=1\" data-thumbnail=\"http://i.ytimg.com/vi/#{video.youtube}/sddefault.jpg\"></a>"
+    else
+      if video.vimeo
+        url = "https://player.vimeo.com/video/#{video.vimeo}"
+      elsif video.wwdc
+        url = "https://developer.apple.com/videos/play/#{video.wwdc}"
+      end
+      "<iframe src=\"#{url}\" frameborder=\"0\" allowfullscreen></iframe>"
     end
-    "<iframe src=\"#{url}\" frameborder=\"0\" allowfullscreen></iframe>"
   end
 end
