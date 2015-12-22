1f20a7aa647a6bb0095d2ffe05b876a8d92cef2f
Chris Eidhof <chris@eidhof.nl>
2015-12-22 10:59:18 +0100

    Another test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd pomotv
 git config --global user.name pomotvbot
 git config --global user.email chris+pomotvbot@eidhof.nl
 git remote set-url origin "https://${GH_TOKEN}@github.com/chriseidhof/pomotv.git"
