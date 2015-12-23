15a768743e624634bfdb6d6dae9cd60f60cc6e89
Chris Eidhof <chris@eidhof.nl>
2015-12-23 10:17:06 +0100

    Try abusing after_success instead of deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-rvm $TRAVIS_RUBY_VERSION do bundle install --jobs=3 --retry=3 --deployment --path=${BUNDLE_PATH:-vendor/bundle}
 git config --global user.name pomotvbot
 git config --global user.email chris+pomotvbot@eidhof.nl
 git remote set-url origin "https://${GH_TOKEN}@github.com/chriseidhof/pomotv.git"
-rvm $TRAVIS_RUBY_VERSION do bundle exec rake publish
+bundle exec rake publish
