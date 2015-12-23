1d55a8e888e625df3c60898ba80d1b5760a10ac0
Chris Eidhof <chris@eidhof.nl>
2015-12-23 10:13:43 +0100

    Testing...

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-rvm use default
-export BUNDLE_GEMFILE=$PWD/Gemfile
+rvm $TRAVIS_RUBY_VERSION do bundle install --jobs=3 --retry=3 --deployment --path=${BUNDLE_PATH:-vendor/bundle}
 git config --global user.name pomotvbot
 git config --global user.email chris+pomotvbot@eidhof.nl
 git remote set-url origin "https://${GH_TOKEN}@github.com/chriseidhof/pomotv.git"
-bundle exec rake publish
+rvm $TRAVIS_RUBY_VERSION do bundle exec rake publish
