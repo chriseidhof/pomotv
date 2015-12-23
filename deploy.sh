c55fa73039c12fe0114ab47cd81ce016bab3a22d
Chris Eidhof <chris@eidhof.nl>
2015-12-23 10:10:52 +0100

    Yet another try.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rvm use default
+export BUNDLE_GEMFILE=$PWD/Gemfile
 git config --global user.name pomotvbot
 git config --global user.email chris+pomotvbot@eidhof.nl
 git remote set-url origin "https://${GH_TOKEN}@github.com/chriseidhof/pomotv.git"
