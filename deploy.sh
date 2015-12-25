c555d681764567ff72bcd4b52cc2bc6177a11e01
Chris Eidhof <chris@eidhof.nl>
2015-12-25 18:21:15 +0100

    Cleanup the build folder only

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -r build
 rvm $TRAVIS_RUBY_VERSION do bundle install --jobs=3 --retry=3 --deployment --path=${BUNDLE_PATH:-vendor/bundle}
 git config --global user.name pomotvbot
 git config --global user.email chris+pomotvbot@eidhof.nl
